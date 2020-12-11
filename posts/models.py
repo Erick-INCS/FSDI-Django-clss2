0479a865e6f1c740fd2cda54f624045967a3bf84
Erick Arroyo <incspnsoft@gmail.com>
2020-12-11 00:32:59 +0000

    static files and some css

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-# Create your models here.
 class Post(models.Model): 
     text = models.TextField()
+    imageURL = models.TextField(null=True)
 
     def __str__(self):
         return self.text[:50]
